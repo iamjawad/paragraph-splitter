d0398eb46ce1c243f771bee3630d34b8c3dc0a12
Jawad Ahmed <ijawadak@gmail.com>
2020-12-11 20:35:58 +0500

    init

---
+++ assets/js/main.js
@@ -0,0 +1,46 @@
+var app = new Vue({
+    el: '#mainapp',
+    data: {
+        test: 0,
+        dir: "ltr",
+        text: "",
+        splitText: [],
+        del: ".",
+        step: 1,
+        selectedSplits: []
+    },
+    methods: {
+        addRow: function(){
+            this.rows.push(this.rowCount);
+            this.rowCount++;
+        },
+        setDir: function(dir){
+            this.dir = dir;
+        },
+        addSelected: function(selected){
+            this.selectedSplits.push(selected);
+        },
+        setStep: function(step){
+            this.step = step;
+        },
+        restart: function(){
+            this.text = "";
+            this.splitText = [];
+            this.selectedSplits = [];
+            this.step = 1;
+        },
+        download: function(){
+            csv = "data:text/csv;charset=utf-8," + this.selectedSplits.join(",");
+            var encodedUri = encodeURI(csv);
+            window.open(csv);
+        },
+        split: function(){
+            if(this.text[this.text.length -1] == this.del){
+                this.text = this.text.slice(0, -1);
+            }
+            this.splitText = this.text.split(this.del);
+            app.text.split('.', app.text.split('.').length - 1)
+            this.setStep(2);
+        }
+    }
+});
